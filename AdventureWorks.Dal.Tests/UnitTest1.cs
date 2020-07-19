9eec149c77cdaaef27b2bbb6b054ed6e6f90af88
Brian Thomas <brian.thomas@btechnical-consulting.com>
2020-07-19 02:19:10 -0500

    Initial project creation

---
+++ AdventureWorks.Dal.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace AdventureWorks.Dal.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ AdventureWorks.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AdventureWorks.Models
+{
+    public class Class1
+    {
+    }
+}
