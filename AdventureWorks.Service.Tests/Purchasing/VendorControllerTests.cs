8af53a6513f9c326f0a4dbe899eb02bffc5f005b
Brian Thomas <brian.thomas@btechnical-consulting.com>
2020-07-31 01:29:31 -0500

    VendorController HttpGet tests are passing

--- AdventureWorks.Service.Tests/Purchasing/VendorControllerTests.cs
+++ AdventureWorks.Service.Tests/Purchasing/VendorControllerTests.cs
@@ -21,7 +21,7 @@ namespace AdventureWorks.Service.Tests.Purchasing
         {
             rootAddress = "api/vendor";
         }
-        
+
         [Fact]
         public async void ShouldGetAllVendors()
         {
@@ -38,12 +38,12 @@ namespace AdventureWorks.Service.Tests.Purchasing
         }
 
         [Theory]
-        [InlineData(2,"Cycles-R-Us")]
-        [InlineData(3,"Desoto Bike Mart")]
-        [InlineData(4,"DFW Bike Resellers")]  
-        [InlineData(5,"Light Speed")]
-        [InlineData(6,"Trikes")]
-        [InlineData(7,"Australia Bike Retailer")]           
+        [InlineData(2, "Cycles-R-Us")]
+        [InlineData(3, "Desoto Bike Mart")]
+        [InlineData(4, "DFW Bike Resellers")]
+        [InlineData(5, "Light Speed")]
+        [InlineData(6, "Trikes")]
+        [InlineData(7, "Australia Bike Retailer")]
         public async void ShouldGetVendorByID(int vendorID, string vendorName)
         {
             ResetDatabase();
@@ -58,17 +58,53 @@ namespace AdventureWorks.Service.Tests.Purchasing
             }
         }
 
+        [Fact]
+        public async void ShouldGetAllVendorViewModels()
+        {
+            ResetDatabase();
+
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync($"{serviceAddress}{rootAddress}/ViewModels");
+                Assert.True(response.IsSuccessStatusCode);
+                var jsonResponse = await response.Content.ReadAsStringAsync();
+                var vendors = JsonConvert.DeserializeObject<List<VendorViewModel>>(jsonResponse);
+                Assert.Equal(6, vendors.Count);
+            }
+        }
+
         [Theory]
-        [InlineData(2,1)]
-        [InlineData(3,2)]
-        [InlineData(4,0)]
-        [InlineData(5,1)]
-        [InlineData(6,1)]        
-        [InlineData(7,1)]
+        [InlineData(2, "Cycles-R-Us")]
+        [InlineData(3, "Desoto Bike Mart")]
+        [InlineData(4, "DFW Bike Resellers")]
+        [InlineData(5, "Light Speed")]
+        [InlineData(6, "Trikes")]
+        [InlineData(7, "Australia Bike Retailer")]
+        public async void ShouldGetVendorViewModelByBizEntityID(int vendorID, string vendorName)
+        {
+            ResetDatabase();
+
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync($"{serviceAddress}{rootAddress}/ViewModels/{vendorID}");
+                Assert.True(response.IsSuccessStatusCode);
+                var jsonResponse = await response.Content.ReadAsStringAsync();
+                var vendor = JsonConvert.DeserializeObject<VendorViewModel>(jsonResponse);
+                Assert.Equal(vendorName, vendor.Name);
+            }
+        }
+
+        [Theory]
+        [InlineData(2, 1)]
+        [InlineData(3, 2)]
+        [InlineData(4, 0)]
+        [InlineData(5, 1)]
+        [InlineData(6, 1)]
+        [InlineData(7, 1)]
         public async void ShouldGetAllAddressesForOneVendor(int vendorID, int numAddresses)
         {
             ResetDatabase();
-            
+
             using (var client = new HttpClient())
             {
                 var response = await client.GetAsync($"{serviceAddress}{rootAddress}/{vendorID}/address");
@@ -76,15 +112,15 @@ namespace AdventureWorks.Service.Tests.Purchasing
 
                 var jsonResponse = await response.Content.ReadAsStringAsync();
                 var addresses = JsonConvert.DeserializeObject<List<Address>>(jsonResponse);
-                Assert.Equal(numAddresses, addresses.Count);  
+                Assert.Equal(numAddresses, addresses.Count);
             }
-        } 
+        }
 
         [Fact]
         public async void ShouldGetOneVendorAddress()
         {
             ResetDatabase();
-            
+
             using (var client = new HttpClient())
             {
                 int addressID = 6;
@@ -95,33 +131,77 @@ namespace AdventureWorks.Service.Tests.Purchasing
                 var jsonResponse = await response.Content.ReadAsStringAsync();
                 var address = JsonConvert.DeserializeObject<Address>(jsonResponse);
                 Assert.Equal("6266 Melody Lane", address.AddressLine1);
-            }            
+            }
         }
 
         [Theory]
-        [InlineData(2,0)]
-        [InlineData(3,1)]
-        [InlineData(4,1)]
-        [InlineData(7,3)]
+        [InlineData(2, 1)]
+        [InlineData(3, 2)]
+        [InlineData(4, 0)]
+        [InlineData(5, 1)]
+        [InlineData(6, 1)]
+        [InlineData(7, 1)]
+        public async void ShouldGetAllAddressViewModelsForOneVendor(int vendorID, int numAddresses)
+        {
+            ResetDatabase();
+
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync($"{serviceAddress}{rootAddress}/{vendorID}/AddressViewModel");
+                Assert.True(response.IsSuccessStatusCode);
+
+                var jsonResponse = await response.Content.ReadAsStringAsync();
+                var addresses = JsonConvert.DeserializeObject<List<AddressViewModel>>(jsonResponse);
+                Assert.Equal(numAddresses, addresses.Count);
+            }
+        }
+
+        [Theory]
+        [InlineData(1, "28 San Marino Ct")]
+        [InlineData(2, "90 Sunny Ave")]
+        [InlineData(3, "298 Sunnybrook Drive")]
+        [InlineData(4, "1900 Desoto Court")]
+        [InlineData(5, "211 East Pleasant Run Rd")]
+        [InlineData(6, "6266 Melody Lane")]
+        public async void ShouldGetOneVendorAddressViewModel(int addressID, string line1)
+        {
+            ResetDatabase();
+
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync($"{serviceAddress}{rootAddress}/AddressViewModel/{addressID}");
+                Assert.True(response.IsSuccessStatusCode);
+
+                var jsonResponse = await response.Content.ReadAsStringAsync();
+                var address = JsonConvert.DeserializeObject<AddressViewModel>(jsonResponse);
+                Assert.Equal(line1, address.AddressLine1);
+            }
+        }
+
+        [Theory]
+        [InlineData(2, 0)]
+        [InlineData(3, 1)]
+        [InlineData(4, 1)]
+        [InlineData(7, 3)]
         public async void ShouldGetAllContactsForEachVendor(int vendorID, int numContacts)
         {
             ResetDatabase();
-            
+
             using (var client = new HttpClient())
             {
-                var response = await client.GetAsync($"{serviceAddress}{rootAddress}/{vendorID}/contact");  
+                var response = await client.GetAsync($"{serviceAddress}{rootAddress}/{vendorID}/contact");
                 Assert.True(response.IsSuccessStatusCode);
                 var jsonResponse = await response.Content.ReadAsStringAsync();
                 var vendorContacts = JsonConvert.DeserializeObject<List<PersonClass>>(jsonResponse);
-                Assert.Equal(numContacts, vendorContacts.Count);                
-            }            
+                Assert.Equal(numContacts, vendorContacts.Count);
+            }
         }
 
         [Fact]
         public async void ShouldGetOneVendorContact()
         {
             ResetDatabase();
-            
+
             using (var client = new HttpClient())
             {
                 int personID = 8;
@@ -132,10 +212,55 @@ namespace AdventureWorks.Service.Tests.Purchasing
                 var contact = JsonConvert.DeserializeObject<PersonClass>(jsonResponse);
                 Assert.Equal("Johnny", contact.FirstName);
                 Assert.Equal("Dough", contact.LastName);
-            }            
+            }
         }
 
+        [Theory]
+        [InlineData(2, 0)]
+        [InlineData(3, 1)]
+        [InlineData(4, 1)]
+        [InlineData(7, 3)]
+        public async void ShouldGetAllContactViewModelsForEachVendor(int vendorID, int numContacts)
+        {
+            ResetDatabase();
 
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync($"{serviceAddress}{rootAddress}/{vendorID}/ContactViewModel");
+                Assert.True(response.IsSuccessStatusCode);
+                var jsonResponse = await response.Content.ReadAsStringAsync();
+                var vendorContacts = JsonConvert.DeserializeObject<List<VendorContactViewModel>>(jsonResponse);
+                Assert.Equal(numContacts, vendorContacts.Count);
+            }
+        }
 
+        [Theory]
+        [InlineData(8, "j.dough@adventure-works.com")]
+        [InlineData(9, "jo3@adventure-works.com")]
+        [InlineData(10, "terri.phide@adventure-works.com")]
+        [InlineData(11, "marie0@adventure-works.com")]
+        [InlineData(12, "william3@adventure-works.com")]
+        [InlineData(13, "paula2@adventure-works.com")]
+        [InlineData(19, "charelene0@adventure-works.com")]
+        public async void ShouldGetOneVendorContactViewModel(int personID, string emailAddress)
+        {
+            ResetDatabase();
+
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync($"{serviceAddress}{rootAddress}/ContactViewModel/{personID}");
+                Assert.True(response.IsSuccessStatusCode);
+
+                var jsonResponse = await response.Content.ReadAsStringAsync();
+                var contact = JsonConvert.DeserializeObject<VendorContactViewModel>(jsonResponse);
+                Assert.Equal(emailAddress, contact.EmailAddress);
+            }
+        }
+
+        // TODO HttpPost tests
+
+        // TODO HttpPut tests
+
+        // TODO HttpDelete tests
     }
 }
--- AdventureWorks.Service/Controllers/VendorController.cs
+++ AdventureWorks.Service/Controllers/VendorController.cs
@@ -11,7 +11,7 @@ namespace AdventureWorks.Service.Controllers
 {
     [Produces("application/json")]
     [Route("api/[controller]")]
-    [ApiController]    
+    [ApiController]
     public class VendorController : ControllerBase
     {
         private readonly IVendorRepo _vendorRepo;
@@ -23,67 +23,130 @@ namespace AdventureWorks.Service.Controllers
 
         [HttpGet]
         [ProducesResponseType(200)]
-        [ProducesResponseType(500)]  
-        public ActionResult<IList<Vendor>> GetAllVendors() => Ok(_vendorRepo.GetAll());            
+        [ProducesResponseType(500)]
+        public ActionResult<IList<Vendor>> GetAllVendors() => Ok(_vendorRepo.GetAll());
 
-        [HttpGet("{vendorID}", Name="GetById")]
+        [HttpGet("{vendorID}", Name = "GetById")]
         [ProducesResponseType(200)]
         [ProducesResponseType(404)]
         [ProducesResponseType(500)]
         public ActionResult<Vendor> GetById(int vendorID)
         {
             var vendor = _vendorRepo.Find(vendorID);
-            
+
             if (vendor == null)
-            { 
+            {
                 return NotFound();
             }
 
             return Ok(vendor);
-        } 
+        }
 
         [HttpGet("{vendorID}/address")]
         [ProducesResponseType(200)]
         [ProducesResponseType(500)]
-        public ActionResult<IList<Address>> GetVendorAddresses(int vendorID) 
+        public ActionResult<IList<Address>> GetVendorAddresses(int vendorID)
             => Ok(_vendorRepo.GetVendorAddresses(vendorID));
 
-        [HttpGet("address/{addressID}", Name="GetVendorAddress")]
+        [HttpGet("address/{addressID}", Name = "GetVendorAddress")]
         [ProducesResponseType(200)]
         [ProducesResponseType(404)]
         [ProducesResponseType(500)]
         public ActionResult<Address> GetVendorAddress(int addressID)
         {
             var address = _vendorRepo.GetVendorAddress(addressID);
-            
+
             if (address == null)
-            { 
+            {
                 return NotFound();
             }
 
-            return Ok(address);            
+            return Ok(address);
         }
 
         [HttpGet("{vendorID}/contact")]
         [ProducesResponseType(200)]
         [ProducesResponseType(500)]
-        public ActionResult<IList<PersonClass>> GetVendorContacts(int vendorID) 
-            => Ok(_vendorRepo.GetVendorContacts(vendorID)); 
+        public ActionResult<IList<PersonClass>> GetVendorContacts(int vendorID)
+            => Ok(_vendorRepo.GetVendorContacts(vendorID));
 
-        [HttpGet("contact/{personID}", Name="GetVendorContact")]
+        [HttpGet("contact/{personID}", Name = "GetVendorContact")]
         [ProducesResponseType(200)]
         [ProducesResponseType(404)]
         [ProducesResponseType(500)]
         public ActionResult<PersonClass> GetVendorContact(int personID)
         {
             PersonClass contact = _vendorRepo.GetVendorContact(personID);
- 
+
             if (contact == null)
-            { 
+            {
                 return NotFound();
             }
 
-            return Ok(contact);            
+            return Ok(contact);
+        }
+
+        [HttpGet("ViewModels")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(500)]
+        public ActionResult<IList<VendorViewModel>> GetVendorViewModels()
+            => Ok(_vendorRepo.GetVendorViewModels());
+
+        [HttpGet("ViewModels/{vendorID}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(500)]
+        public ActionResult<VendorViewModel> GetVendorViewModel(int vendorID)
+        {
+            var vendor = _vendorRepo.GetVendorViewModel(vendorID);
+            if (vendor != null)
+            {
+                return Ok(vendor);
+            }
+
+            return NotFound();
+        }
+
+        [HttpGet("{vendorID}/AddressViewModel")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(500)]
+        public ActionResult<IList<AddressViewModel>> GetVendorAddressViewModels(int vendorID)
+            => Ok(_vendorRepo.GetVendorAddressViewModels(vendorID));
+
+        [HttpGet("AddressViewModel/{addressID}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(500)]
+        public ActionResult<AddressViewModel> GetVendorAddressViewModel(int addressID)
+        {
+            var address = _vendorRepo.GetAddressViewModel(addressID);
+            if (address != null)
+            {
+                return Ok(address);
+            }
+
+            return NotFound();
+        }
+
+        [HttpGet("{vendorID}/ContactViewModel")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(500)]
+        public ActionResult<IList<VendorContactViewModel>> GetVendorContactViewModels(int vendorID)
+            => Ok(_vendorRepo.GetVendorContactViewModels(vendorID));
+
+        [HttpGet("ContactViewModel/{personID}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(500)]
+        public ActionResult<PersonClass> GetVendorContactViewModel(int personID)
+        {
+            var contact = _vendorRepo.GetVendorContactViewModel(personID);
+            if (contact != null)
+            {
+                return Ok(contact);
+            }
+
+            return NotFound();
         }
 
         [HttpPost]
@@ -96,11 +159,11 @@ namespace AdventureWorks.Service.Controllers
             {
                 return BadRequest();
             }
-            
+
             var vendorID = _vendorRepo.Add(vendor);
 
-            return CreatedAtAction(nameof(GetById), new { vendorID = vendorID },vendor);
-        } 
+            return CreatedAtAction(nameof(GetById), new { vendorID = vendorID }, vendor);
+        }
 
 
 
